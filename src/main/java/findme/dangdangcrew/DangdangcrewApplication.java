96580b0c5cd6c5b4bbc7bb9b1fd0fa3cc7e6b131
leedidu <dlwltn510@gmail.com>
2025-02-06 05:29:00 +0900

    initial commit

---
+++ src/main/java/findme/dangdangcrew/DangdangcrewApplication.java
@@ -0,0 +1,13 @@
+package findme.dangdangcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangdangcrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangdangcrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/findme/dangdangcrew/DangdangcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package findme.dangdangcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdangcrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
