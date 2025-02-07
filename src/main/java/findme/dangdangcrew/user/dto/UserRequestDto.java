abb61009a0c7632ec551a77c2f8e0c7607bcb25e
ijnim1121 <jimin3347@naver.com>
2025-02-07 10:29:59 +0900

    fix: 리뷰 반영(message 오타 수정)

--- src/main/java/findme/dangdangcrew/user/dto/UserRequestDto.java
+++ src/main/java/findme/dangdangcrew/user/dto/UserRequestDto.java
@@ -16,7 +16,7 @@ public class UserRequestDto {
     private String email;
 
     @Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다.")
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
