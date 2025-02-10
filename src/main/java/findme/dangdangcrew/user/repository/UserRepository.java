6e675e56a5a497a8802c6617b5e75f04c15457f5
taemin <jungtaemin1214@naver.com>
2025-02-10 13:28:32 +0900

    feat: 평가 점수 user 엔티티의 유저 점수 반영, user entity, repository 수정

--- src/main/java/findme/dangdangcrew/user/repository/UserRepository.java
+++ src/main/java/findme/dangdangcrew/user/repository/UserRepository.java
@@ -2,6 +2,9 @@ package findme.dangdangcrew.user.repository;
 
 import findme.dangdangcrew.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
     boolean existsByPhoneNumber(String phoneNumber);
+
+    @Modifying
+    @Query("UPDATE User u SET u.userScore = :newScore WHERE u.id = :userId")
+    void updateUserScore(@Param("userId") Long userId, @Param("newScore") Double newScore);
 }
