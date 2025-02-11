184f388fa4046aebb6f324f7798424552696913b
youngkwanglim <richard08@naver.com>
2025-02-11 10:13:45 +0900

    feat: ChatRoom 엔티티에 제목과 방 개설자 필드 추가

--- src/main/java/findme/dangdangcrew/chat/entity/ChatRoom.java
+++ src/main/java/findme/dangdangcrew/chat/entity/ChatRoom.java
@@ -1,9 +1,13 @@
 package findme.dangdangcrew.chat.entity;
 
+import findme.dangdangcrew.user.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,6 +22,12 @@ public class ChatRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
 
     private static final int MAX_PARTICIPANTS = 20;
     private int currentParticipants = 0;
