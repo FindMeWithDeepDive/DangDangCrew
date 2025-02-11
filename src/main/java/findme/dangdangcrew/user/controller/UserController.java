47173a9dcf8e403dc655ba597359e7de87852b4e
ijnim1121 <jimin3347@naver.com>
2025-02-11 18:05:44 +0900

    feat: 토큰 갱신 API 추가

--- src/main/java/findme/dangdangcrew/user/controller/UserController.java
+++ src/main/java/findme/dangdangcrew/user/controller/UserController.java
@@ -1,9 +1,6 @@
 package findme.dangdangcrew.user.controller;
 
-import findme.dangdangcrew.user.dto.LoginRequestDto;
-import findme.dangdangcrew.user.dto.TokenResponseDto;
-import findme.dangdangcrew.user.dto.UserRequestDto;
-import findme.dangdangcrew.user.dto.UserResponseDto;
+import findme.dangdangcrew.user.dto.*;
 import findme.dangdangcrew.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -32,6 +29,13 @@ public class UserController {
     @Operation(summary = "로그인", description = "이메일과 비밀번호를 입력받아 JWT 토큰을 발급합니다.")
     public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto request) {
         return ResponseEntity.ok(userService.authenticate(request));
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "토큰 갱신", description = "Redis에 저장된 Refresh Token과 비교하여 검증 후 새로운 Access Token을 발급합니다.")
+    public ResponseEntity<TokenResponseDto> refreshAccessToken(@RequestBody TokenRefreshRequestDto request) {
+        TokenResponseDto response = userService.refreshAccessToken(request);
+        return ResponseEntity.ok(response);
     }
 
 }
