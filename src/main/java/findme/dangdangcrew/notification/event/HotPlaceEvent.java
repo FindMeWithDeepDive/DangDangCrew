6c8992bbe2699bd00ef11de62f82c39baf530d87
sukangpunch <popora99@naver.com>
2025-02-11 11:50:37 +0900

    fix: eventlistner 수정 - #23

--- src/main/java/findme/dangdangcrew/notification/event/HotPlaceEvent.java
+++ src/main/java/findme/dangdangcrew/notification/event/HotPlaceEvent.java
@@ -4,7 +4,7 @@ package findme.dangdangcrew.notification.event;
  * @param placeName 핫한 장소 이름
  */
 public record HotPlaceEvent(
-        Long targetUserId,
         String placeName,
-        String placeId) {
+        String placeId
+) {
 }
