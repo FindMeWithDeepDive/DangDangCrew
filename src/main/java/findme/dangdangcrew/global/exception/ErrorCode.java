7791a7fed3c63e7eae209121559cd790c75479d0
youngkwanglim <richard08@naver.com>
2025-02-13 16:50:01 +0900

    feat: 커스텀 에러코드 추가

--- src/main/java/findme/dangdangcrew/global/exception/ErrorCode.java
+++ src/main/java/findme/dangdangcrew/global/exception/ErrorCode.java
@@ -30,6 +30,9 @@ public enum ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     INVALID_JSON_FORMAT(HttpStatus.BAD_REQUEST, "요청한 JSON 형식이 올바르지 않습니다."),
 
+    // Chat Error
+    CHAT_ROOM_FULL(HttpStatus.BAD_REQUEST, "채팅방이 가득 찼습니다. (최대 20명)"),
+    CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다."),
     ;
 
     private final HttpStatus status;
