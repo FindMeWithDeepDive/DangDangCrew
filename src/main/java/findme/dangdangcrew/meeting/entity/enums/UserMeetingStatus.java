c534729bed605b6cdc9d729f8ac95a33df567cd4
leedidu <dlwltn510@gmail.com>
2025-02-13 13:46:36 +0900

    feat: UserMeeting status 추가

--- src/main/java/findme/dangdangcrew/meeting/entity/enums/UserMeetingStatus.java
+++ src/main/java/findme/dangdangcrew/meeting/entity/enums/UserMeetingStatus.java
@@ -4,5 +4,6 @@ public enum UserMeetingStatus {
     CANCELLED, // 예약취소
     CONFIRMED, // 확정
     WAITING, // 신청 후 대기 중
-    LEADER // 모임 생성자
+    LEADER, // 모임 생성자
+    MEETING_DELETED // 모임이 삭제 됐을 경우
 }
