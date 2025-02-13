cfdd4c1cbfb42f952ed283e9c91194610d4d0b0a
youngkwanglim <richard08@naver.com>
2025-02-13 14:22:11 +0900

    fix: 클래스명 수정

--- src/main/java/findme/dangdangcrew/chat/service/ChatMessageService.java
+++ src/main/java/findme/dangdangcrew/chat/service/ChatMessageService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class MessageService {
+public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
 
