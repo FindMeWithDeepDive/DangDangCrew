2b4df8deae76d2eb8214186f0d6760141c8e9573
youngkwanglim <richard08@naver.com>
2025-02-13 17:05:53 +0900

    feat: 채팅방 중복 입장 확인

--- src/main/java/findme/dangdangcrew/chat/service/ChatRoomService.java
+++ src/main/java/findme/dangdangcrew/chat/service/ChatRoomService.java
@@ -3,10 +3,13 @@ package findme.dangdangcrew.chat.service;
 import findme.dangdangcrew.chat.dto.ChatMessageRequestDto;
 import findme.dangdangcrew.chat.dto.ChatMessageRequestDto.MessageType;
 import findme.dangdangcrew.chat.entity.ChatRoom;
+import findme.dangdangcrew.chat.repository.ChatParticipantRepository;
 import findme.dangdangcrew.chat.repository.ChatRoomRepository;
 import findme.dangdangcrew.global.exception.CustomException;
 import findme.dangdangcrew.global.exception.ErrorCode;
 import findme.dangdangcrew.meeting.entity.Meeting;
+import findme.dangdangcrew.user.entity.User;
+import findme.dangdangcrew.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,10 +21,12 @@ public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageService chatMessageService;
+    private final UserService userService;
+    private final ChatParticipantRepository chatParticipantRepository;
 
-    public String processChat(ChatMessageRequestDto chatMessage, Long roomId) {
+    public String processChat(ChatMessageRequestDto chatMessage, Long roomId, Long userId) {
         if (chatMessage.getType() == ChatMessageRequestDto.MessageType.ENTER) {
-            enterRoom(roomId);
+            enterRoom(roomId, userId);
             return chatMessage.getSender() + "님이 입장하셨습니다.";
         }
         if (chatMessage.getType() == ChatMessageRequestDto.MessageType.LEAVE) {
@@ -32,8 +37,11 @@ public class ChatRoomService {
         return chatMessage.getMessage();
     }
 
-    public void enterRoom(Long roomId) {
+    public void enterRoom(Long roomId, Long userId) {
         ChatRoom chatRoom = getChatRoom(roomId);
+        User user = userService.getUser(userId);
+        checkIfAlreadyJoined(chatRoom, user);
+
         if (!chatRoom.addParticipant()) {
             throw new CustomException(ErrorCode.CHAT_ROOM_FULL);
         }
@@ -48,6 +56,12 @@ public class ChatRoomService {
         ChatRoom chatRoom = chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new CustomException(ErrorCode.CHAT_ROOM_NOT_FOUND));
         return chatRoom;
+    }
+
+    public void checkIfAlreadyJoined(ChatRoom chatRoom, User user) {
+        if (chatParticipantRepository.findByChatRoomAndUser(chatRoom, user).isPresent()) {
+            throw new CustomException(ErrorCode.ALREADY_JOINED);
+        }
     }
 
     public void createChatRoom(Meeting meeting) {
