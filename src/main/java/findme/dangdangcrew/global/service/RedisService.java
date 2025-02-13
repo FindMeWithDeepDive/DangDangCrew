a041251462b34c45586e2f193b5a801d11e8f06e
sukangpunch <popora99@naver.com>
2025-02-13 17:27:32 +0900

    부하를 줄이기 위해 RedisService 수정

--- src/main/java/findme/dangdangcrew/global/service/RedisService.java
+++ src/main/java/findme/dangdangcrew/global/service/RedisService.java
@@ -2,9 +2,12 @@ package findme.dangdangcrew.global.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -37,19 +40,29 @@ public class RedisService {
     }
 
 
-    // 장소별 참가자 수 증가
+    // 장소별 참가자 수 증가, 좀더 정확하게 count값 비교가 아닌, ttl_lock을 설정하여 더 명확한 동작을 하도록 수정
     public void incrementHotPlace(String placeId) {
         String key = HOTPLACE_PREFIX + placeId;
         Long count = redisTemplate.opsForValue().increment(key);
 
-        if(count == 1){
-            redisTemplate.expire(key,EXPIRATION_TIME, TimeUnit.SECONDS);
+        // 이 과정중에 ttl_lock:{placeId} 이라는 키가 없으면 생성한다.
+        Boolean isNewKey = redisTemplate.opsForValue().setIfAbsent("ttl_lock:" + placeId, "1", EXPIRATION_TIME, TimeUnit.SECONDS);
+        if (Boolean.TRUE.equals(isNewKey)) {
+            redisTemplate.expire(key, EXPIRATION_TIME, TimeUnit.SECONDS);
         }
     }
 
-    // 핫 플레이스 관련 key 조회
-    public Set<String> getHotPlaceKeys(){
-        return redisTemplate.keys(HOTPLACE_PREFIX+"*");
+    // 핫 플레이스 관련 key 조회 - cursor 방식으로 부하를 줄이는 방법 - 약 50개씩 차근차근 조회
+    public Set<String> scanHotPlaceKeys() {
+        Cursor<byte[]> cursor = redisTemplate.getConnectionFactory()
+                .getConnection()
+                .scan(ScanOptions.scanOptions().match(HOTPLACE_PREFIX + "*").count(50).build());
+
+        Set<String> keys = new HashSet<>();
+        while (cursor.hasNext()) {
+            keys.add(new String(cursor.next()));
+        }
+        return keys;
     }
 
     // 특정 장소의 참가자 수 조회
--- src/main/java/findme/dangdangcrew/place/service/HotPlaceService.java
+++ src/main/java/findme/dangdangcrew/place/service/HotPlaceService.java
@@ -5,6 +5,8 @@ import findme.dangdangcrew.global.service.RedisService;
 import findme.dangdangcrew.notification.event.HotPlaceEvent;
 import findme.dangdangcrew.place.domain.Place;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.parameters.P;
@@ -16,18 +18,22 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class HotPlaceService {
-    private static final int HOTPLACE_THRESHOLD = 2;
+    private static final int HOTPLACE_THRESHOLD = 3;
 
 
     private final EventPublisher eventPublisher;
     private final PlaceService placeService;
     private final RedisService redisService;
 
-    @Scheduled(fixedRate = 10000)
+    // fixedRate는 중복 실행의 위험이 있어, fixedDelay를 통해 작업이 끝남을 기준으로 처리
+    @Scheduled(fixedDelay = 10000)
     public void checkHotPlaces(){
         // redis에서 hotplace: 로 시작하는 모든 키 조회
-        Set<String> keys = redisService.getHotPlaceKeys();
+        log.info("[checkHotPlace] 핫 플레이스 크론 실행");
+        Set<String> keys = redisService.scanHotPlaceKeys();
+        log.info("keys 값 개수 : {}", keys.size());
         if(keys == null || keys.isEmpty()) return;
 
         for(String key : keys){
