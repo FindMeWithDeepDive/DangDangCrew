b8cd1e17be956156b2106c8e0f51e4236bdb4943
youngkwanglim <richard08@naver.com>
2025-02-13 16:30:19 +0900

    feat: ChatParticipant 엔티티 생성

---
+++ src/main/java/findme/dangdangcrew/chat/entity/ChatParticipant.java
@@ -0,0 +1,40 @@
+package findme.dangdangcrew.chat.entity;
+
+import findme.dangdangcrew.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+@Table(name = "chat_participant", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"room_id", "user_id"})
+})
+public class ChatParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
