f76b9facb758ebf1526f3e3718ff2ee896213c41
sukangpunch <popora99@naver.com>
2025-02-13 11:39:10 +0900

    feat: HotPlaceService 작성

--- src/main/java/findme/dangdangcrew/place/service/HotPlaceService.java
+++ src/main/java/findme/dangdangcrew/place/service/HotPlaceService.java
@@ -9,8 +9,12 @@ import org.springframework.stereotype.Service;
 public class HotPlaceService {
 
     private static final String HOTPLACE_PREFIX = "hotplace:";
+    private static final String NOTIFIED_PREFIX = "hotplace-notified";
     private static final int HOTPLACE_THRESHOLD = 10;
 
     private final RedisTemplate<String, String> redisTemplate;
+
+
+
 
 }
