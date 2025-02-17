845791041978e8c23d4e3d3b95930437d145cb84
ijnim1121 <jimin3347@naver.com>
2025-02-18 00:05:34 +0900

    feat: 서비스 코드 동작을 위한 dto 수정

--- src/main/java/findme/dangdangcrew/user/dto/ChangePasswordRequestDto.java
+++ src/main/java/findme/dangdangcrew/user/dto/ChangePasswordRequestDto.java
@@ -1,11 +1,13 @@
 package findme.dangdangcrew.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordRequestDto {
     @NotBlank(message = "현재 비밀번호를 입력해주세요.")
     private String currentPassword;
